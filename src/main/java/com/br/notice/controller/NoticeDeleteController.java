ce6b529dba648f83bc41fdade8239f37ef6e1111
bitna_lee <lbn063017@naver.com>
2023-01-11 02:15:39 +0900

    230110 수업
    공지사항_상세페이지, 수정, 삭제

---
+++ src/main/java/com/br/notice/controller/NoticeDeleteController.java
@@ -0,0 +1,56 @@
+package com.br.notice.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.br.notice.model.service.NoticeService;
+import com.br.notice.model.vo.Notice;
+
+/**
+ * Servlet implementation class NoticeDeleteController
+ */
+@WebServlet("/delete.no")
+public class NoticeDeleteController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NoticeDeleteController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int noticeNo = Integer.parseInt(request.getParameter("no")); // 조회하고자했던 공지사항번호
+		
+		int result = new NoticeService().deleteNotice(noticeNo);
+		
+		if(result > 0) { 
+			request.getSession().setAttribute("alertMsg", "성공적으로 공지사항이 삭제되었습니다.");
+			response.sendRedirect(request.getContextPath() + "/list.no");
+			
+		} else {
+			request.setAttribute("errorMsg", "공지사항 삭제 실패");
+			request.getRequestDispatcher("views/common/errorPage.jsp").forward(request, response);
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
